32acc9c72a398f38cbd5a4636e4322fd7d2bc431
veller <vellerbauer@gmail.com>
2023-08-22 08:52:15 -0300

    feat(index.js): add support for sorting column names alphabetically

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 
-const csv = require('async-csv');
-const fs = require('fs');
+const csv = require('async-csv')
+const fs = require('fs')
 
 /**
  * Converts an array of objects into a CSV file.
@@ -13,16 +13,16 @@ class ObjectsToCsv {
    */
   constructor(objectArray) {
     if (!Array.isArray(objectArray)) {
-      throw new Error('The input to objects-to-csv must be an array of objects.');
+      throw new Error('The input to objects-to-csv must be an array of objects.')
     }
 
     if (objectArray.length > 0) {
-      if (objectArray.some(row => typeof row !== 'object')) {
-        throw new Error('The array must contain objects, not other data types.');
+      if (objectArray.some((row) => typeof row !== 'object')) {
+        throw new Error('The array must contain objects, not other data types.')
       }
     }
 
-    this.data = objectArray;
+    this.data = objectArray
   }
 
   /**
@@ -32,55 +32,57 @@ class ObjectsToCsv {
    * @param {boolean} [options.append] - Whether to append to file. Default is overwrite (false).
    * @param {boolean} [options.bom] - Append the BOM mark so that Excel shows
    * @param {boolean} [options.allColumns] - Whether to check all items for column names or only the first.  Default is the first.
+   * @param {boolean} [options.sortColumns] - Whether to sort columns names alphabetically. If false, maintains
+   *  the order of keys in the object
    * Unicode correctly.
    */
   async toDisk(filename, options) {
     if (!filename) {
-      throw new Error('Empty filename when trying to write to disk.');
+      throw new Error('Empty filename when trying to write to disk.')
     }
 
-    let addHeader = false;
+    let addHeader = false
 
     // If the file didn't exist yet or is empty, add the column headers
     // as the first line of the file. Do not add it when we are appending
     // to an existing file.
-    const fileNotExists = !fs.existsSync(filename) || fs.statSync(filename).size === 0;
+    const fileNotExists = !fs.existsSync(filename) || fs.statSync(filename).size === 0
     if (fileNotExists || !options || !options.append) {
-      addHeader = true;
+      addHeader = true
     }
 
-    const allColumns = options && options.allColumns
-      ? options.allColumns
-      : false;
+    const allColumns = options && options.allColumns ? options.allColumns : false
 
-    let data = await this.toString(addHeader, allColumns);
+    const sortColumns = options.sortColumns ? options.sortColumns : true
+
+    let data = await this.toString(addHeader, allColumns, sortColumns)
     // Append the BOM mark if requested at the beginning of the file, otherwise
     // Excel won't show Unicode correctly. The actual BOM mark will be EF BB BF,
     // see https://stackoverflow.com/a/27975629/6269864 for details.
     if (options && options.bom && fileNotExists) {
-      data = '\ufeff' + data;
+      data = '\ufeff' + data
     }
 
     if (options && options.append) {
       return new Promise((resolve, reject) => {
         fs.appendFile(filename, data, 'utf8', (error) => {
           if (error) {
-            reject(error);
+            reject(error)
           } else {
-            resolve(data);
+            resolve(data)
           }
-        });
-      });
+        })
+      })
     } else {
       return new Promise((resolve, reject) => {
         fs.writeFile(filename, data, 'utf8', (error) => {
           if (error) {
-            reject(error);
+            reject(error)
           } else {
-            resolve(data);
+            resolve(data)
           }
-        });
-      });
+        })
+      })
     }
   }
 
@@ -91,9 +93,11 @@ class ObjectsToCsv {
    * @param {boolean} allColumns - Whether to check all items for column names.
    *   Uses only the first item if false.
    * @returns {Promise<string>}
+   * @param {boolean} sortColumns - Whether to sort columns names alphabetically. If false, maintains
+   *  the order of keys in the object
    */
-  async toString(header = true, allColumns = false) {
-    return await convert(this.data, header, allColumns);
+  async toString(header = true, allColumns = false, sortColumns = true) {
+    return await convert(this.data, header, allColumns, sortColumns)
   }
 }
 
@@ -102,40 +106,41 @@ class ObjectsToCsv {
  * @param {object[]} data
  * @param {boolean} header - Whether the first line should contain column headers.
  * @param {boolean} allColumns - Whether to check all items for column names.
+ * @param {boolean} [options.sortColumns] - Whether to sort columns names alphabetically. If false, maintains
+ *  the order of keys in the object
  *   Uses only the first item if false.
  * @returns {string}
  */
-async function convert(data, header = true, allColumns = false) {
+async function convert(data, header = true, allColumns = false, sortColumns = true) {
   if (data.length === 0) {
-    return '';
+    return ''
   }
 
-  const columnNames =
-    allColumns
-      ? [...data
-        .reduce((columns, row) => { // check each object to compile a full list of column names
-          Object.keys(row).map(rowKey => columns.add(rowKey));
-          return columns;
-        }, new Set())]
-      : Object.keys(data[0]); // just figure out columns from the first item in array
+  const columnNames = allColumns
+    ? [
+        ...data.reduce((columns, row) => {
+          // check each object to compile a full list of column names
+          Object.keys(row).map((rowKey) => columns.add(rowKey))
+          return columns
+        }, new Set()),
+      ]
+    : Object.keys(data[0]) // just figure out columns from the first item in array
 
-  if (allColumns) {
-    columnNames.sort(); // for predictable order of columns
+  if (allColumns && sortColumns) {
+    columnNames.sort() // for predictable order of columns
   }
 
   // This will hold data in the format that `async-csv` can accept, i.e.
   // an array of arrays.
-  let csvInput = [];
+  let csvInput = []
   if (header) {
-    csvInput.push(columnNames);
+    csvInput.push(columnNames)
   }
 
   // Add all other rows:
-  csvInput.push(
-    ...data.map(row => columnNames.map(column => row[column])),
-  );
+  csvInput.push(...data.map((row) => columnNames.map((column) => row[column])))
 
-  return await csv.stringify(csvInput);
+  return await csv.stringify(csvInput)
 }
 
-module.exports = ObjectsToCsv;
+module.exports = ObjectsToCsv
